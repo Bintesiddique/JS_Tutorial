a9383c2809112f3cb5a45ddaead3fa5d54567d24
Areeba Siddique <areebasiddique021@gmail.com>
2024-08-28 05:00:12 -0700

    import files

---
+++ lecture_01/variables.js
@@ -0,0 +1,25 @@
+
+// Variables in JS
+// => Variables are containers for data
+
+// Variable Rules
+// * Variable names are case sensitive; "a" & "A" is different.
+// * Only letters, digits, underscore(_) and $ is allowed. (not even space)
+// * Only a letter, underscore() or $ should be 1st character.
+// * Reserved words cannot be variable names.
+
+// string value
+
+// fullName = "Areeba  s";
+
+// numbers value
+age = 20;
+// null value
+x = null;
+// undefined value
+y = undefined;
+// by using let
+let fullName = "Ariba";
+// by using constant variable
+PI = 3.14
+console.log(PI);
