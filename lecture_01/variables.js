86dd184e59332b3f5b0babb944760b334853b94a
Areeba Siddique <areebasiddique021@gmail.com>
2024-08-28 08:42:14 -0700

    Update variables.js

--- lecture_01/variables.js
+++ lecture_01/variables.js
@@ -11,15 +11,23 @@
 // string value
 
 // fullName = "Areeba  s";
+// console.log(fullName);
 
 // numbers value
 age = 20;
+console.log(age);
 // null value
 x = null;
+console.log(x);
 // undefined value
 y = undefined;
+console.log(y);
 // by using let
 let fullName = "Ariba";
+console.log(fullName);
+// by using var
+var f_name ="Areeba";
+console.log(f_name);
 // by using constant variable
-PI = 3.14
+const PI = 3.14
 console.log(PI);
