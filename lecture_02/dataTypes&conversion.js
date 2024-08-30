8e7d73a24c73890ea27bd10df216ddff49e9dfbe
Areeba Siddique <areebasiddique021@gmail.com>
2024-08-30 09:09:35 -0700

    Add files via upload

---
+++ lecture_02/dataTypes&conversion.js
@@ -0,0 +1,54 @@
+
+// Primitive DataType
+
+// numbers value
+age = 20;
+// null value
+x = null; //absance of object:
+// undefined value
+y = undefined;
+// bigint value
+let c = BigInt("123")
+// console.log(c);
+// string value
+fullName = "areeba";
+// console.log(fullName);
+//symbol value
+let i = Symbol("Hello !");
+
+console.log(i);
+
+//bool value
+doWork = true;
+// console.log(doWork);
+
+
+
+// conversion
+
+// means convert one datatype to another:
+
+let a=10;
+a = true;
+a = "new srtring";
+// console.log(a);
+
+// =>concatination
+
+const value1 = '5';
+const value2 = 10;
+
+// let sum= value1 + value2;
+// console.log(sum);
+
+// => explict conversion
+
+sum =Number(value1) + value2;
+// console.log(sum);
+
+
+//Types of conversion:
+
+// => String
+// =>Boolean
+// =>numbers
